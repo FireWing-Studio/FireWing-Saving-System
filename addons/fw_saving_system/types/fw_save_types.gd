cee7943583432ef9ce16130fe62dc5b23944fee4
Samuele Benassai <77284041+tucanofasano@users.noreply.github.com>
2026-07-21 00:06:55 +0200

    Created core types

---
+++ addons/fw_saving_system/types/fw_save_types.gd
@@ -0,0 +1,57 @@
+
+class_name FWSaveTypes
+extends RefCounted
+
+enum Error {
+	OK = 2000
+}
+
+enum Encoding {
+	INVALID = 0,
+
+	BINARY = 1,
+	BINARY_ZSTD = 2
+}
+
+enum HeaderFlags {
+	NONE = 0
+}
+
+class Result extends RefCounted:
+	var value: Variant
+	var error: Error
+	
+	func _init(val, err):
+		value = val
+		error = err
+	
+	func is_ok():
+		return error == null || error == Error.OK
+
+	static func success(val: Variant) -> Result:
+		return Result.new(val, null)
+
+	static func failure(err: Error) -> Result:
+		return Result.new(null, err)
+
+class Header extends RefCounted:
+	var magic_number: StringName
+	var version: int
+	var toc_count: int
+	var flags: HeaderFlags
+
+	var game_literal: StringName
+	var game_version: StringName
+	var godot_version: StringName
+
+	var toc_checksum: PackedByteArray
+	var header_checksum: PackedByteArray
+
+class TocEntry extends RefCounted:
+	var key: StringName
+
+	var payload_offset: int
+	var payload_disk_size: int
+	var payload_uncompressed_size: int
+	
+	var payload_checksum: PackedByteArray
